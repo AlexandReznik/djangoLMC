9a58537e290740506793336b081e899ea35a88f8
AlexandReznik <saha.reznik2004@gmail.com>
2022-10-23 20:34:05 +0300

    Done tasks

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from mainapp.models import News, Course, Lesson, CourseTeacher
+
+admin.site.register(News)
+admin.site.register(Course)
+admin.site.register(Lesson)
+admin.site.register(CourseTeacher)
