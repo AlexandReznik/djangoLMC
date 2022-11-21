1fe5761759abac8c4ee4e0ebb8993cadeb37ffe8
AlexandReznik <saha.reznik2004@gmail.com>
2022-11-21 20:05:00 +0100

    Edit form

--- authapp/views.py
+++ authapp/views.py
@@ -13,43 +13,27 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 import os
 from django.contrib.auth import get_user_model
 from authapp import forms
+from django.contrib.auth.mixins import UserPassesTestMixin
+from django.views.generic import UpdateView
 
 
-class ProfileEditView(LoginRequiredMixin, TemplateView):
-    template_name = "authapp/profile_edit.html"
-    login_url = reverse_lazy("authapp:login")
+class ProfileEditView(UserPassesTestMixin, UpdateView):
+    template_view = 'authapp/profile_edit.html'
+    model = get_user_model()
+    form_class = forms.CustomUserChangeForm
 
-    def post(self, request, *args, **kwargs):
-        try:
-            if request.POST.get("username"):
-                request.user.username = request.POST.get("username")
-            if request.POST.get("first_name"):
-                request.user.first_name = request.POST.get("first_name")
-            if request.POST.get("last_name"):
-                request.user.last_name = request.POST.get("last_name")
-            if request.POST.get("age"):
-                request.user.age = request.POST.get("age")
-            if request.POST.get("email"):
-                request.user.email = request.POST.get("email")
-            if request.FILES.get("avatar"):
-                if request.user.avatar and os.path.exists(
-                    request.user.avatar.path
-                ):
-                    os.remove(request.user.avatar.path)
-                request.user.avatar = request.FILES.get("avatar")
-            request.user.save()
-            messages.add_message(request, messages.INFO, _("Saved!"))
-        except Exception as exp:
-            messages.add_message(
-                request,
-                messages.WARNING,
-                mark_safe(f"Something goes worng:<br>{exp}"),
-            )
-        return HttpResponseRedirect(reverse_lazy("authapp:profile_edit"))
+    def test_func(self):
+        return True if self.request.user.pk == self.kwargs.get("pk") else False
+
+    def get_success_url(self):
+        return reverse_lazy("authapp:profile_edit", args=[self.request.user.pk])
+
+    def get_object(self, queryset=None):
+        return self.request.user
 
 
 class RegisterView(CreateView):
-    template_name = "authapp/customer_form.html"
+    template_name = "authapp/customuser_form.html"
     model = get_user_model()
     form_class = forms.CustomUserCreationForm
     success_url = reverse_lazy("mainapp:mainapp")
